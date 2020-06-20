bc02638ebd394c46de7d0e270f5cafbcce6e0799
blazelisk <bzk@DESKTOP-9R61Q3S.localdomain>
2020-06-20 16:34:31 -0400

    Created Production Branch

--- src/components/mainmenu.js
+++ src/components/mainmenu.js
@@ -13,9 +13,9 @@ export default class MainMenu extends Component{
               <ul>
                 <li className="active"><a href="#" data-nav-section="home">Home</a></li>
                 <li><a href="#about" data-nav-section="about">About</a></li>
+                <li><a href="#education" data-nav-section="education">Education</a></li>
                 <li><a href="#" data-nav-section="services">Services</a></li>
                 <li><a href="#" data-nav-section="skills">Skills</a></li>
-                <li><a href="#education" data-nav-section="education">Education</a></li>
                 <li><a href="#experience" data-nav-section="experience">Experience</a></li>
                 <li><a href="#" data-nav-section="work">Work</a></li>
                 <li><a href="#" data-nav-section="blog">Blog</a></li>
