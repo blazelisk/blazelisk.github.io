cfa09cfdc39389bb114791e43a88ad8e6413f729
blazelisk <bzk@DESKTOP-9R61Q3S.localdomain>
2020-06-21 18:45:20 -0400

    Vim test

--- src/components/about.js
+++ src/components/about.js
@@ -12,7 +12,8 @@ export default class About extends Component {
                   <div className="col-md-12">
                     <div className="about-desc">
                       <h2 className="colorlib-heading">About Me</h2>
-                      <p><strong>Hi, I'm Benjamin Bui.</strong> I love exploring both how evolving technology changes how we model and analyze the increasingly complex systems present in the world. Automation and technological innovation have the potential for fundamental change in a variety of core ideas in economics such as price discrimination and labor markets which warrants further investigation. It is this intersection of technology, economics, and their effects on human lives that is my primary field of interest.</p>
+                      <p><strong>Hi, I'm Benjamin Bui.</strong> I love exploring both how evolving technology changes how we model and analyze the increasingly complex systems present in the world. Automation and technological innovation have the potential for fundamental change in a variety of core ideas in economics such as price discrimination and labor markets which warrants further investigation. It is this intersection of technology, economics, and their effects on human lives that is my primary field of interest. </p>
+                      <p> After receiving my undergraduate degree, I began to pursue a master's degree in education from Johns Hopkins University. I left the program after a year however as I felt that the vocation lacked </p>
                     </div>
                   </div>
                 </div>
