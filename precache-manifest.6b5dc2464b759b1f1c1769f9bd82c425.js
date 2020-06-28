bc63988bb93f538ecf428bd337758a4b39d0bc3c
blazelisk <benjaminbui97@gmail.com>
2020-06-28 19:23:31 -0400

    Updates

---
+++ precache-manifest.6b5dc2464b759b1f1c1769f9bd82c425.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ca6faa1f35183c43bac0e07e5e6a68f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ac296cd44c34753c33e8",
+    "url": "/static/css/main.f4bc1fdf.chunk.css"
+  },
+  {
+    "revision": "b114995882098f9d9557",
+    "url": "/static/js/2.61fdbed7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.61fdbed7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac296cd44c34753c33e8",
+    "url": "/static/js/main.541f652e.chunk.js"
+  },
+  {
+    "revision": "36436507306ed8af6883",
+    "url": "/static/js/runtime-main.5af3524f.js"
+  }
+]);
